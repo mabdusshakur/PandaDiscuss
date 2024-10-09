179d99a4c3fb7076c1105b8864f80cbcb338456a
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-10-09 17:20:59 +0600

    Add Auth Routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+/*
+|--------------------------------------------------------------------------
+| Authentication Routes
+|--------------------------------------------------------------------------
+|
+| This routes handles, userLogin, verifyOtp, userLogout 
+|
+*/
+
+Route::post('/login', [AuthController::class, 'userLogin']);
+Route::post('/verify', [AuthController::class, 'verifyOtp'])->name('verify');
+Route::post('/logout', [AuthController::class, 'userLogout'])->name('logout');
