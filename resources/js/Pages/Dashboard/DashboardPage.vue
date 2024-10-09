a2ae2cd530f627f5aed960fcf50b23726b4fba0d
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-10-10 00:23:40 +0600

    implement reverse emits, and component props

--- resources/js/Pages/Dashboard/DashboardPage.vue
+++ resources/js/Pages/Dashboard/DashboardPage.vue
@@ -1,14 +1,33 @@
 <script setup>
 import UserList from '../../Components/Dashboard/UserList.vue';
 import ChatWindow from '../../Components/Dashboard/ChatWindow.vue';
+
+import { ref } from 'vue';
+
+// inputs
+const showChatWindow = ref(false);
+const conversationId = ref(0);
+
+
+// Function to open chat window
+const openChatWindow = (id) => {
+    console.log('open chat window', id);
+    conversationId.value = id;
+    showChatWindow.value = true;
+};
+
 </script>
 <template>
     <div>
         <div class="flex h-screen bg-gradient bg-gradient-to-t from-white to-pink-100">
-            <UserList />
+
+            <!-- UserList Component, openChatWindow is a emit -->
+            <UserList @openChatWindow="openChatWindow" />
 
             <main class="w-1/6 flex-1 overflow-auto bg-pink-50 p-4 ">
-                <ChatWindow />
+
+                <!-- ChatWindow Component, showChatWindow and conversationId are props -->
+                <ChatWindow :showChatWindow="showChatWindow" :conversationId="conversationId" />
             </main>
         </div>
     </div>
