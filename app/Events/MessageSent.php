d9c0b9ba5002ba3e0ce995a0277c780136d72245
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-10-11 16:49:36 +0600

    Create message sent Event

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,50 @@
+<?php
+// app/Events/MessageSent.php
+
+namespace App\Events;
+
+use App\Models\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Message $message
+     * @return void
+     */
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('conversation.' . $this->message->conversation_id);
+    }
+
+    public function broadcastAs()
+    {
+        return 'MessageSent';
+    }
+
+    public function broadcastWith()
+    {
+        return ['message' => $this->message];
+    }
+}
