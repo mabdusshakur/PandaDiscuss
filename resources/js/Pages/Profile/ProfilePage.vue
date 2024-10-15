815d2d5cb04d97b6e0cda5f060dbb5ef20cb4d5b
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-10-15 13:56:40 +0600

    Implement, UpdateProfile Form

--- resources/js/Pages/Profile/ProfilePage.vue
+++ resources/js/Pages/Profile/ProfilePage.vue
@@ -1,19 +1,8 @@
 <script setup>
+import ProfileForm from '../../Components/Profile/ProfileForm.vue';
+
 </script>
 <template>
-    <div class="flex justify-center items-center h-screen bg-gradient bg-gradient-to-t from-white to-pink-100">
-        <div class="p-2 shadow-lg rounded-lg">
-            <div class="flex flex-col sm:flex-row gap-3">
-                <input type="text"
-                    class="block w-full rounded-md border-pink-300 shadow-sm focus:border-pink-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-pink-50 disabled:text-pink-500 p-2"
-                    placeholder="Name" />
-                <input type="text"
-                    class="block w-full rounded-md border-pink-300 shadow-sm focus:border-pink-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 cursor-not-allowed disabled:bg-pink-100 disabled:text-pink-500 p-2"
-                    placeholder="Email" readonly />
-            </div>
-            <button type="button"
-                class=" mt-2 rounded-lg border border-pink-500 bg-pink-500 px-3 py-1.5 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-pink-600 hover:bg-pink-600 focus:ring focus:ring-pink-200 disabled:cursor-not-allowed disabled:border-pink-300 disabled:bg-pink-300">Save</button>
-        </div>
-    </div>
+    <ProfileForm />
 </template>
 <style scoped></style>
